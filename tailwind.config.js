fbf50da4372d788685d127964c32bbf2453d4a97
Jony <jonyleung@outlook.com>
2025-01-24 16:34:29 +0800

    🔧 build: update package dependencies and configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{ts,tsx}'],
+  content: ["./src/**/*.{ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-  darkMode: ['class', '.figma-dark']
-}
+  darkMode: ["class", ".figma-dark"],
+};
